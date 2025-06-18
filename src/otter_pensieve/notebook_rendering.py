0c645604f9bc79ebb68218bbc32aae172aadf11d
Marlon Rondinelli <marlon@marlonrondinelli.com>
2025-06-18 11:52:10 -0700

    use web pdf exporter again

--- src/otter_pensieve/notebook_rendering.py
+++ src/otter_pensieve/notebook_rendering.py
@@ -9,9 +9,9 @@ def render_notebook(notebook: nbformat.NotebookNode) -> bytes:
 
     nbconvert.TemplateExporter.extra_template_basedirs = [str(TEMPLATE_DIR)]
     orig_template_name = nbconvert.TemplateExporter.template_name
-    nbconvert.TemplateExporter.template_name = "via_latex_xecjk"
+    nbconvert.TemplateExporter.template_name = "via_html"
     try:
-        exporter = cast(nbconvert.PDFExporter, nbconvert.PDFExporter())
+        exporter = cast(nbconvert.WebPDFExporter, nbconvert.WebPDFExporter())
         pdf, _ = nbconvert.export(exporter, notebook)
     finally:
         nbconvert.TemplateExporter.template_name = orig_template_name
